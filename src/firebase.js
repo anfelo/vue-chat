ca0b20c83e05f613e9869d380dbb68e1b8a50e91
Andres Osorio <af.osorio1341@gmail.com>
2020-08-26 12:16:26 -0500

    Remove firebase configs

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBW878au3rxLfEqF-e4oYnH8cmrMXtv16A',
-	authDomain: 'vue-chat-7ee30.firebaseapp.com',
-	databaseURL: 'https://vue-chat-7ee30.firebaseio.com',
-	projectId: 'vue-chat-7ee30',
-	storageBucket: 'vue-chat-7ee30.appspot.com',
-	messagingSenderId: '244547660152',
-	appId: '1:244547660152:web:4cb4e33c4ae518b394008d'
+	apiKey: '***********************',
+	authDomain: '***********************',
+	databaseURL: '***********************',
+	projectId: '***********************',
+	storageBucket: '***********************',
+	messagingSenderId: '***********************',
+	appId: '***********************'
 };
 
 firebase.initializeApp(firebaseConfig);
